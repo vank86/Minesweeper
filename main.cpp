d941024df28a58ddc838fbcb6722ca89e4082564
Ivan Hancharou <264303@student.pwr.edu.pl>
2023-04-04 19:53:45 +0200

    SFML added; revealField() isn't fixed still
    1. Need to solve the problem with detecting mines with
     [row-1][col-1] etc., when it out of height\width.
    
    2. revealField doesn't work perfectly.
    
    3. To check 2 new classes View and Ctrl.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "MinesweeperBoard.h"
 #include "MSBoardTextView.h"
 #include "MSTextController.h"
+#include <SFML/Graphics.hpp>
+
 //git connection check
 
 int main() {
@@ -15,8 +17,44 @@ int main() {
     view.display();
     MSTextController ctrl ( board, view );
 
-    ctrl.play();
+//    ctrl.play();
 
+
+/*------------SFML-----------*/
+
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window", sf::Style::Default);
+
+    sf::CircleShape shape(20.f, 300);
+
+    // set the shape color to green
+    shape.setFillColor(sf::Color(100, 250, 50));
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::White);
+
+        // draw everything here...
+        shape.setPosition(10, 10);
+        window.draw(shape);
+
+        shape.setPosition(100, 100);
+        window.draw(shape);
+
+        // end the current frame
+        window.display();
+    }
 
 //    std::cout << "\n\n\n";
 //    MinesweeperBoard b(5,7, HARD);
